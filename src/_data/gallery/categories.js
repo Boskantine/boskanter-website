ee3d1287b324e0eeb022b985a587ad9473d1d19c
Boskantine <161729746+Boskantine@users.noreply.github.com>
2026-04-12 16:25:47 +0200

    Update categories.js

--- src/_data/gallery/categories.js
+++ src/_data/gallery/categories.js
@@ -6,8 +6,32 @@ const categories = [
         title: {en: "All categories", fr: "Toutes les catégories", nl: "Alle categorieën"}
     },
     {
-        key: "something",
-        title: {en: "something", fr: "Quelquechose", nl: "iets"}
+        key: "accomodation",
+        title: {en: "accomodation", fr: "accomodation", nl: "accomodatie"}
+    },
+    {
+        key: "activities",
+        title: {en: "activities", fr: "activités", nl: "activiteiten"}
+    },
+    {
+        key: "volunteers",
+        title: {en: "volunteers", fr: "volontairs", nl: "vrijwilligers"}
+    },
+    {
+        key: "garden",
+        title: {en: "garden", fr: "jardin", nl: "tuin"}
+    },
+    {
+        key: "omgeving",
+        title: {en: "environment", fr: "environnement", nl: "omgeving"}
+    },
+    {
+        key: "food",
+        title: {en: "food", fr: "nutrition", nl: "voedsel"}
+    },
+    {
+        key: "crafts",
+        title: {en: "crafts", fr: "artisanat", nl: "ambachten"}
     }
 ]
 
